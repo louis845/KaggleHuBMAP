Fixed some bugs, optimized computation of confidence class for separated focal loss.

model_unet_attention.py
@@ -119,7 +119,7 @@ class UNetClassifier(torch.nn.Module):
         assert (not squeeze_excitation) or use_res_conv, "residual convolutions must be used if squeeze_excitation is True"
         assert (not atrous_outconv_split) or (use_atrous_conv and (num_classes > 1)), "atrous_outconv_split can only be used if use_atrous_conv and num_classes > 1"
         self.backbone = model_unet_base.UNetBackbone(in_channels, hidden_channels, use_batch_norm=use_batch_norm, use_res_conv=use_res_conv, pyr_height=pyr_height, res_conv_blocks=res_conv_blocks, bottleneck_expansion=bottleneck_expansion, squeeze_excitation=squeeze_excitation, use_initial_conv=use_initial_conv)
-        self.classifier = UNetEndClassifier(hidden_channels, use_batch_norm=use_batch_norm, use_atrous_conv=use_atrous_conv, pyr_height=pyr_height, deep_supervision=deep_supervision, num_classes=num_classes, num_deep_multiclasses=num_deep_multiclasses, bottleneck_expansion=bottleneck_expansion)
+        self.classifier = UNetEndClassifier(hidden_channels, use_batch_norm=use_batch_norm, use_atrous_conv=use_atrous_conv, atrous_outconv_split=atrous_outconv_split, pyr_height=pyr_height, deep_supervision=deep_supervision, num_classes=num_classes, num_deep_multiclasses=num_deep_multiclasses, bottleneck_expansion=bottleneck_expansion)
         self.pyr_height = pyr_height
 
     def forward(self, x):

reconstructed_model_progressive_supervised_unet.py
@@ -27,8 +27,7 @@ import logging_memory_utils
 def compute_confidence_class(result: torch.Tensor):
     with torch.no_grad():
         if use_separated_focal_loss:
-            sigmoid = torch.sigmoid(result[:, 0, ...])
-            return (sigmoid <= 0.5) * (torch.argmax(result[:, 1:, ...], dim=1) + 1)
+            return (result[:, 0, ...] <= 0.0) * (torch.argmax(result[:, 1:, ...], dim=1) + 1)
         else:
             softmax = torch.softmax(result, dim=1)
             # (confidence level at least 0.5)